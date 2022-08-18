450e49469bb7051cf7b121c35a8999af56896278
Davyd Kachor <96185501+davidkachor@users.noreply.github.com>
2022-08-18 22:28:14 +0300

    Add error res of /api/* req

--- api/src/routes/api.router.ts
+++ api/src/routes/api.router.ts
@@ -11,5 +11,8 @@ apiRouter.get('/', (req, res) => {
 	res.status(200).json({ links: `${protocol}://${origin}/api/links` })
 })
 apiRouter.use('/links', linkRouter)
+apiRouter.get('/*', (req, res) => {
+	res.status(400).json({ status: 400, error: 'This model does not exist' })
+})
 
 export default apiRouter
